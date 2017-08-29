5ca92e909860b31773f1812cc2656593e332e207
Eipeks <apexzdgr8est@gmail.com>
2017-08-29 13:03:43 +0100

    First commit

---
+++ app/src/main/java/i/am/eipeks/x_plore/_activities/Home.java
@@ -0,0 +1,15 @@
+package i.am.eipeks.x_plore._activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import i.am.eipeks.x_plore.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
